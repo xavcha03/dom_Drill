2b864d470e64f5cb5faa617de42bdc0be4c4fc9d
xavcha03 <ch.xavier03@gmail.com>
2022-08-04 10:43:48 +0200

    Add .important class on imortant franchise

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+//params
+let franchiseToFind = "Fast and Furious";
+
 //Object de référence
 let ulElt = document.querySelector("ul");
 let children = [];
@@ -8,5 +11,13 @@ if (ulElt.hasChildNodes()) {
 
 //Loop on children
 for (child of children) {
-  console.log(child);
+  //Test if current element of loop is a node
+  if (child.nodeType === 1) console.log(child);
+
+  //Test if current element contain the franchiseToFind
+  let importantFranchise = null;
+  if (child.innerHTML === franchiseToFind) {
+    importantFranchise = child;
+    importantFranchise.classList.add("important");
+  }
 }
