d86c0598172e839639a42b914c85371cf803fa1d
xavcha03 <ch.xavier03@gmail.com>
2022-08-04 16:42:01 +0200

    Finalisation de l'exercice

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,50 @@ if (ulElt.hasChildNodes()) {
 //Delete duplicate nodes
 cleanList(children);
 
-//Random the content
-randomElt(ulElt);
-
 //put the important franchise to top of list
 putImportantAtFirst(ulElt, franchiseToFind);
 
+//Ajout d'un event au body
+addEventToBody();
 
+//Create new diw before list
+let newDiv = document.createElement("div");
+//Création de l'élément Select
+let selectElt = document.createElement("select");
+//creation des option du select
+let option = document.createElement("option");
+option.value = "important franchises";
+option.innerHTML = "important franchises";
+selectElt.appendChild(option);
+option = document.createElement("option");
+option.value = "normal franchises";
+option.innerHTML = "normal franchises";
+selectElt.appendChild(option);
+
+//Delete duplicate nodes
+cleanList(children);
+//Ajout de l'eventListenner sur le select
+selectElt.addEventListener("change", (e) => {
+	for (child of children) {
+		if (
+			child.classList.contains("important") &&
+			e.target.value == "important franchises"
+		) {
+			child.style.visibility = "visible";
+		} else if (
+			!child.classList.contains("important") &&
+			e.target.value == "normal franchises"
+		) {
+			child.style.visibility = "visible";
+		} else {
+			child.style.visibility = "hidden";
+		}
+	}
+});
+
+newDiv.appendChild(selectElt);
+
+ulElt.parentNode.insertBefore(newDiv, ulElt);
 
 //Loop on children
 for (child of children) {
@@ -52,7 +89,8 @@ function randomElt(parentElt) {
 	children = parentElt.childNodes;
 	//parcours et mélange
 	for (let i = 0; i < children.length; i++) {
-		let randNum = Math.round(Math.random() * children.length);
+		let randNum = Math.round(Math.random() * (children.length - 1));
+		console.log(randNum);
 		if (children[i].nodeType == 1) {
 			parentElt.prepend(children[randNum]);
 		}
@@ -67,7 +105,6 @@ function putImportantAtFirst(parentElement, eltToSearch) {
 	for (elt of listElt) {
 		if (elt.innerHTML === eltToSearch) {
 			//deplacement to the first element
-			console.log(listElt);
 			parentElement.prepend(elt);
 		}
 	}
@@ -75,10 +112,12 @@ function putImportantAtFirst(parentElement, eltToSearch) {
 
 //Reçoit une liste de node et supprime les élément en double
 function cleanList(listElt) {
-	let nodesArr = [...listElt];
+	let nodesArr = listElt;
 	//Premier parcours
 	for (let i = 0; i < nodesArr.length; i++) {
-		let currentContent = nodesArr[i].innerHTML;
+		//si l'élement courant n'est pas un node, on le supprime
+		if (nodesArr[i].nodeType != 1) nodesArr[i].remove();
+
 		//second parcour depuis la position courrante
 		for (let j = i + 1; j < nodesArr.length - 1; j++) {
 			//test si les 2 elements sont identique
@@ -87,4 +126,25 @@ function cleanList(listElt) {
 			}
 		}
 	}
+}
+
+function addEventToBody() {
+	let bodyElt = document.querySelector("body");
+
+	bodyElt.addEventListener("keyup", (e) => {
+		switch (e.key) {
+			case "r":
+				randomElt(ulElt);
+				putImportantAtFirst(ulElt, franchiseToFind);
+				break;
+			case "d":
+				let importantFranchise = [...children].find((elt) => {
+					return elt.innerHTML == franchiseToFind;
+				});
+
+				let clone = importantFranchise.cloneNode(true);
+				console.log(clone);
+				break;
+		}
+	});
 }
